7bc0316bb00957da1a407f360747ff7b356405a6
wfh <897890755@qq.com>
2023-03-07 17:38:07 +0800

    缓存的入门使用1....................

--- learn-redis/redis-demo/src/main/java/com/example/redis/demo/service/impl/UserServiceImpl.java
+++ learn-redis/redis-demo/src/main/java/com/example/redis/demo/service/impl/UserServiceImpl.java
@@ -11,6 +11,15 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /**
+ *
+ * redis 缓存的使用
+ *
+ * @Cacheable
+ * 该注解标注的方法每次被调用前都会触发缓存校验，校验指定参数的缓存是否已存在（已发生过相同参数的调用），若存在，直接返回缓存结果，否则执行方法内容，最后将方法执行结果保存到缓存中。
+ * @CachePut
+ * 它会每次调用方法，然后将缓存写到redis缓存中，并将结果返回。与@Cacheable不同的是，它不会检测在相同Cache中是否存在相同key的缓存元素。
+ * @CacheEvict
+ * 如果缓存中存在存在目标值，则将其从缓存中删除
  * @author wfh
  * @create 2023/3/7 15:14
  */
@@ -36,5 +45,6 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
         return userMapper.selectById(id);
     }
 
+  
 
 }
