2e6f8d652b117d7cc31a0d01966e183caee2d53c
wfh <897890755@qq.com>
2023-03-07 19:17:31 +0800

    缓存的入门使用....................

--- learn-redis/redis-demo/src/main/java/com/example/redis/demo/RedisDemoApplication.java
+++ learn-redis/redis-demo/src/main/java/com/example/redis/demo/RedisDemoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
+/**
+ * http://localhost:8080/swagger-ui.html#/user-controller/updateUsingPOST
+ */
 @SpringBootApplication
 @EnableCaching
 public class RedisDemoApplication {
