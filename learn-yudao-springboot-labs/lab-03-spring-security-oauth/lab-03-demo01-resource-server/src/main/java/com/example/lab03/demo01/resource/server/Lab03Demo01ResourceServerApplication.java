fde73b8862c14c7a49ee2a83bdb1b560a24de1ac
wfh <897890755@qq.com>
2023-04-18 16:45:31 +0800

    很少项目会直接采用 Spring Security OAuth 框架，而是自己参考它进行 OAuth2.0 的实现。并且，一般只会实现密码授权模式。

--- learn-yudao-springboot-labs/lab-03-spring-security-oauth/lab-03-demo01-resource-server/src/main/java/com/example/lab03/demo01/resource/server/Lab03Demo01ResourceServerApplication.java
+++ learn-yudao-springboot-labs/lab-03-spring-security-oauth/lab-03-demo01-resource-server/src/main/java/com/example/lab03/demo01/resource/server/Lab03Demo01ResourceServerApplication.java
@@ -3,6 +3,9 @@ package com.example.lab03.demo01.resource.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 很少项目会直接采用 Spring Security OAuth 框架，而是自己参考它进行 OAuth2.0 的实现。并且，一般只会实现密码授权模式。
+ */
 @SpringBootApplication
 public class Lab03Demo01ResourceServerApplication {
 
