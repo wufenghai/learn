f80faab5771f5f5a0c45b12998fcf80763bf5323
wfh <897890755@qq.com>
2023-05-07 21:05:02 +0800

    队列

---
+++ learn-data-structure-and-algorithms/learn-data-Structure/learn-data-Structure-from-han/src/main/java/com/example/learn/data/structure/from/han/_02队列/_02模拟环形队列/core/CircleArrayQueueDemo.java
@@ -0,0 +1,146 @@
+package com.example.learn.data.structure.from.han._02队列._02模拟环形队列.core;
+
+
+import java.util.Scanner;
+
+public class CircleArrayQueueDemo {
+
+    public static void main(String[] args) {
+        //创建一个队列
+        CircleArrayQueue queue = new CircleArrayQueue(4);//最大是3，留一个位置
+        char key = ' ';//接收用户输入
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        //输出一个菜单
+        while (loop) {
+            System.out.println("s(show):显示队列");
+            System.out.println("e(exit):退出程序");
+            System.out.println("a(add):添加数据到队列");
+            System.out.println("g(get):从队列取出数据");
+            System.out.println("h(head):查看队列头的数据");
+            key = scanner.next().charAt(0);//接收一个字符
+            switch (key) {
+                case 's':
+                    queue.showQueue();
+                    break;
+                case 'a':
+                    System.out.println("输出一个数据");
+                    int value = scanner.nextInt();
+                    queue.addQueue(value);
+                    break;
+                case 'g':
+                    try {
+                        int res = queue.getQueue();
+                        System.out.printf("取出数据是%d", res);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'h':
+                    try {
+                        int res = queue.peek();
+                        System.out.printf("队列头的数据是%d", res);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'e':
+                    scanner.close();
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        System.out.println("程序退出");
+    }
+}
+
+class CircleArrayQueue {
+    private int maxSize;//数组的最大容量
+    // front 变量的含义做一个调整： front 就指向队列的第一个元素, 也就是说 arr[front] 就是队列的第一个元素
+    //front 的初始值 = 0
+    private int front;
+    //rear 变量的含义做一个调整：rear 指向队列的最后一个元素的后一个位置. 因为希望空出一个空间做为约定.
+    //rear 的初始值 = 0
+    private int rear;
+    private int[] arr;//该数组用于存放数据，模拟队列
+
+    public CircleArrayQueue(int arrMaxSize) {
+        maxSize = arrMaxSize;
+        arr = new int[maxSize];
+    }
+
+    //判断队列是否满
+    public boolean isFull() {
+        return (rear + 1) % maxSize == front;
+    }
+
+    //判断队列是否为空
+    public boolean isEmpty() {
+        return front == rear;
+    }
+
+    //添加数据到队列
+    public void addQueue(int n) {
+        //判断队列是否满
+        if (isFull()) {
+            System.out.println("队列满，不能加入数据");
+            return;
+        }
+        //直接将数据加入
+        arr[rear] = n;
+        //将rear后移，但是需要让rear取模
+        rear = (rear + 1) % maxSize;
+    }
+
+    //获取队列的数据，数据出队列
+    public int getQueue() {
+        //判断队列是否为空
+        if (isEmpty()) {
+            //通过抛出异常处理
+            throw new RuntimeException("队列为空，不能取数据");
+        }
+        //这里需要分析 front是指向队列的第一个元素
+        //1.先把front 对应的值保存到一个临时变量
+        int temp = arr[front];
+        //2.将front 后移,考虑取模
+        front = (front + 1) % maxSize;
+        //3.将临时保存的变量返回
+        return temp;
+    }
+
+    //显示队列的所有数据
+    public void showQueue() {
+        //遍历
+        if (isEmpty()) {
+            System.out.println("队列空，没有数据");
+            return;
+        }
+        //思路：从front开始遍历，遍历多少个元素
+        //
+        for (int i = front; i < size(); i++) {
+            System.out.printf("arr[%d]=%d\n", i % maxSize, arr[i % maxSize]);
+        }
+    }
+
+    //求出当前队列的有效数据的个数
+    public int size() {
+        //rear = 2
+        //front = 1
+        //maxSize = 3
+        return (rear + maxSize - front) % maxSize;
+    }
+
+    //显示队列的头数据，不是取出数据
+    public int peek() {
+        //判断队列是否为空
+        if (isEmpty()) {
+            //通过抛出异常处理
+            throw new RuntimeException("队列为空，不能取数据");
+        }
+        return arr[front];
+    }
+
+}
