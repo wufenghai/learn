26b933a8f2cd06e934135c6a8b9c6a34a26f1ea2
wfh <897890755@qq.com>
2023-05-20 09:44:24 +0800

    单一职责原则

---
+++ learn-design-pattern/learn-design-pattern-han/src/main/java/com/example/learn/design/pattern/han/LearnDesignPatternHanApplication.java
@@ -0,0 +1,13 @@
+package com.example.learn.design.pattern.han;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnDesignPatternHanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnDesignPatternHanApplication.class, args);
+    }
+
+}
