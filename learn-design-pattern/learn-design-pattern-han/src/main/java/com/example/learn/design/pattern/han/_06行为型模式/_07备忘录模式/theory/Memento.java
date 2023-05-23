dabd531402fcd73ff6f5a2b8c263101412690325
wfh <897890755@qq.com>
2023-05-23 22:53:41 +0800

    备忘录模式

---
+++ learn-design-pattern/learn-design-pattern-han/src/main/java/com/example/learn/design/pattern/han/_06行为型模式/_07备忘录模式/theory/Memento.java
@@ -0,0 +1,18 @@
+package com.example.learn.design.pattern.han._06行为型模式._07备忘录模式.theory;
+
+public class Memento {
+    private String state;
+
+
+    public Memento(String state) {
+        super();
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+
+
+}
