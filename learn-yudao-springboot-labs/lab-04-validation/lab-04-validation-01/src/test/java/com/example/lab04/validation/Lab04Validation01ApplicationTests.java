044533a914b7935f52add3885883b870f14549ca
wfh <897890755@qq.com>
2023-06-02 19:25:33 +0800

    校验

---
+++ learn-yudao-springboot-labs/lab-04-validation/lab-04-validation-01/src/test/java/com/example/lab04/validation/Lab04Validation01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab04.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab04Validation01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
